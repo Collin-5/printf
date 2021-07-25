2d4c1730eefb8aad8bdabd947f93a202546b33bd
Emediong Francis <emediongfrancis@gmail.com>
2021-07-25 11:35:54 -0700

    updated readme and header file.

--- holberton.h
+++ holberton.h
@@ -1,7 +1,31 @@
+
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* printf functions */
-int _printf(const char *format, ...);
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* _printf */
+int _printf(const char *format, ...);
 #endif
