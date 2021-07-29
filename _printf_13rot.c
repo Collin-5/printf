b9d4ba59ab31ba8793b9d704e447abf9c432741f
Collin-5 <collinsotieno380@gmail.com>
2021-07-29 00:55:09 -0700

    adding

--- _printf_13rot.c
+++ _printf_13rot.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _printf_rot13 - printf str to ROT13 place into buffer
- * @args: type struct va_arg where is allocated printf arguments
+ * @args: type struct va_arg where is allocated printf arguments.
  * Return: counter
  *
  */
