1b217c3b55a39db4fbdc0a4e973c9d1a5a895e28
Collin-5 <collinsotieno380@gmail.com>
2021-07-29 00:53:32 -0700

    rectification

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -1,7 +1,7 @@
 
 #include "holberton.h"
 /**
- * printf_exclusive_string - print exclusuives string.
+ * printf_exclusive_string - print exclusuives string
  * @val: argumen t.
  * Return: the length of the string.
  */
